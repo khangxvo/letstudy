6426b86858d0f02dfc7478661ccef99080479b2d
Khang Vo <victoriouskhan@berkeley.edu>
2024-08-15 23:31:29 -0700

    finished homepage

--- letstudy-ui/src/App.tsx
+++ letstudy-ui/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage';
 
 function App() {
     return (
-        <LoginPage />
+        // <LoginPage />
+        < HomePage />
     );
 }
 
--- letstudy-ui/src/pages/HomePage.tsx
+++ letstudy-ui/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import '../styles/HomePage.css'
 
 interface Task {
     id: number; // TODO Change this to hextring later
@@ -10,10 +11,30 @@ interface Task {
 const HomePage = () => {
 
     // Define state variables and their set functions
-    const [tasks, setTasks] = useState<Task[]>([])
+    const [tasks, setTasks] = useState<Task[]>([
+        {
+            id: 1,
+            title: "test note 1",
+            content: "bla bla note1",
+            hour: 1
+        },
+        // Additional Note objects with id, title, and content properties
+        {
+            id: 2,
+            title: "test note 2 ",
+            content: "bla bla note2",
+            hour: 1
+        },
+        {
+            id: 3,
+            title: "test note 3",
+            content: "bla bla note3",
+            hour: 1
+        }
+    ])
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
-    const [hour, setHour] = useState(1)
+    const [hour, setHour] = useState<number | 0>(0)
     const [selectedTask, setSelectedTask] = useState<Task | null>(null)
 
     // useEffect(() => {}) //TODO: update this to get task from the backend
@@ -32,7 +53,7 @@ const HomePage = () => {
         setHour(1)
     }
 
-    const handleAddTask = async (event: React.FocusEvent) => {
+    const handleAddTask = async (event: React.FormEvent) => {
         event.preventDefault()
 
         //TODO add function to add task to the endpoint
@@ -48,7 +69,7 @@ const HomePage = () => {
         handleCancel()
     }
 
-    const handleUpdateTask = async (event: React.FocusEvent) => {
+    const handleUpdateTask = async (event: React.FormEvent) => {
         event.preventDefault()
 
         if (!selectedTask) {
@@ -69,5 +90,72 @@ const HomePage = () => {
                 ? updatedTask
                 : task
         )
+
+        setTasks(updatedTaskList)
+        handleCancel()
     }
+
+    const deleteTask = async (event: React.MouseEvent, taskId: number) => {
+        event.stopPropagation()
+
+        const updatedTaskList = tasks.filter((task) => task.id !== taskId)
+
+        setTasks(updatedTaskList)
+    }
+
+    return (
+        <div className='app-container'>
+            <form className='task-form'
+                onSubmit={(event) => { selectedTask ? handleUpdateTask(event) : handleAddTask(event) }}>
+
+                <input
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                    placeholder='Title'
+                    required
+                />
+
+                <textarea
+                    placeholder='Content'
+                    value={content}
+                    onChange={(event) => setContent(event.target.value)}
+                ></textarea>
+
+                <input
+                    type="number"
+                    onChange={(event) => setHour(Number(event.target.value))}
+                    placeholder='hour'
+                    required
+                />
+
+                {
+                    selectedTask ? (
+                        <div className='edit-buttons'>
+                            <button type='submit'>Save</button>
+                            <button onClick={handleCancel}>Cancel</button>
+
+                        </div>
+                    ) : (
+                        <button type='submit'>Add Task</button>
+                    )
+                }
+            </form>
+
+            <div className='tasks-grid'>
+                {tasks.map((task) => (
+                    <div className='task-item' onClick={() => handleTaskClick(task)}>
+                        <div className='task-header'>
+                            <button onClick={(event) => deleteTask(event, task.id)}>x</button>
+
+                        </div>
+                        <h2>{task.title}</h2>
+                        <p>{task.content}</p>
+                        <p>{task.hour}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
+
+export default HomePage
